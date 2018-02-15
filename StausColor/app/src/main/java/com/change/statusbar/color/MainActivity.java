927622db86e98abc57f98cb5d296bed2a87e4426
babarmustafaawan <babarmustafaawan@gmail.com>
2018-02-15 18:42:27 +0500

    chnages of status bar color in specific activity

---
+++ StausColor/app/src/main/java/com/change/statusbar/color/MainActivity.java
@@ -0,0 +1,32 @@
+package com.change.statusbar.color;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Window window =getWindow();
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setStatusBarTintColor(getResources().getColor(R.color.black));
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            MainActivity.this.getWindow().setStatusBarColor(getResources().getColor(R.color.black));
+        }
+    }
+}
